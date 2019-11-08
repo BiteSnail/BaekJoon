3b26c53481333521668f2ca1dd98d5d1940259b8
BiteSnail <rkwoal216@gmail.com>
2019-11-08 13:38:19 +0900

    why??

--- BaekJoon/2156.c
+++ BaekJoon/2156.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 
 int wine[10001];
-int mount[10001];
+int mount[2][10001];
 
 int comp1(int a,int b){
 	return a > b ? a : b;
-}
-
-int solve(int n, int flag){
-	if(n<1) return 0;
-
-	if(mount[n]>0) return mount[n];
-	else{
-		solve(n-1,1);
-		//printf("mount[%d]: %d -> ",n, mount[n]);
-		if(flag<2){
-			mount[n]=comp1(mount[n], wine[n]+solve(n-1, flag+1));
-			//printf("%d",mount[n]);
-		}
-		mount[n]=comp1(mount[n], wine[n]+solve(n-2, 1));
-		//printf("%d -> ",mount[n]);
-		
-		//printf("\n");
-	}
-
-	return mount[n];
 }
 
 int main(void){
 	int n;
 	int re;
 	for(int i=0;i<10001;i++){
-		mount[i]=0;
+		mount[0][i]=mount[1][i]=0;
 	}
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++){
 		scanf("%d",&wine[i]);
 	}
 	
-	for(int i=1;i<=n;i++)
-		printf("%d: %d\n",i, mount[i]);
-	re=solve(n, 1);
-	for(int i=1;i<=n;i++)
-		printf("%d: %d\n",i, mount[i]);
+	mount[0][1]=wine[1];
+	for(int i=2;i<=n;i++){
+		mount[0][i]=comp1(mount[0][i-2],mount[1][i-2])+wine[i];
+		if(i>2)
+			mount[0][i]=comp1(mount[0][i], comp1(mount[0][i-3], mount[0][i-3])+wine[i]);
+		mount[1][i]=mount[0][i-1]+wine[i];
+	}
+	
+	for(int i=1;i<=n;i++){
+		printf("[%d %d]\n",mount[0][i], mount[1][i]);
+	}
+
+	re=comp1(mount[0][n], mount[1][n]);
+	re=comp1(re, comp1(mount[0][n-1],mount[1][n-1]));
 	printf("%d\n",re);	
 
 }
