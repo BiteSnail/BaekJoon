18962b96833d7cf1d3e22108320cbce537e4212e
bitesnail <rkwoal216@gmail.com>
2022-07-28 19:17:51 +0900

    ProblemL

--- JUNGOL/ProblmeL.cpp
+++ JUNGOL/ProblmeL.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
-    
+    string str;
+    int sum = 0;
+    int cur_wood_amount=0;
+    cin >> str;
+    for(int i=0;i<str.size();i++){
+        if(str[i]==')'){
+            cur_wood_amount--;
+            sum += (str[i-1]=='(') ? cur_wood_amount : 1;
+        }
+        else{
+            cur_wood_amount++;
+        }
+    }
+
+    cout<< sum;
+
     return 0;
 }
